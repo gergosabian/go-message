709649a030c206d6871564466d3d4ff441380d8e
Gergő Sábián <gergosabian@gmail.com>
2024-03-16 20:29:54 +0100

    Add basic chat functionality

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,117 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"log"
+	"html/template"
+	"io"
 	"net/http"
+	"sync"
 
-	"github.com/gergosabian/go-message/pkg/router"
+	"golang.org/x/net/websocket"
 )
 
 func main() {
-	// Setup router
-	r := router.SetupRouter()
+	server := NewServer()
+	chatroom := NewChatroom()
+	page := NewPage(chatroom)
 
-	// Start server
-	fmt.Println("Server started on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	http.Handle("/chat", websocket.Handler(func(ws *websocket.Conn) {
+		server.HandleChat(ws, chatroom)
+	}))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t := template.Must(template.New("index").ParseFiles("templates/index.html", "templates/chatroom.html"))
+		if err := t.Execute(w, *page); err != nil {
+			fmt.Println("Error executing template:", err)
+			return
+		}
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
+
+type Server struct {
+	conns map[*websocket.Conn]bool
+	mutex sync.Mutex
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) HandleChat(ws *websocket.Conn, chatroom *Chatroom) {
+	fmt.Println("New connection:", ws.RemoteAddr())
+	s.mutex.Lock()
+	s.conns[ws] = true
+	s.mutex.Unlock()
+	defer func() {
+		s.mutex.Lock()
+		delete(s.conns, ws)
+		s.mutex.Unlock()
+	}()
+
+	for {
+		var message Message
+
+		if err := websocket.JSON.Receive(ws, &message); err != nil {
+			if err == io.EOF {
+				fmt.Println("Connection closed:", ws.RemoteAddr())
+				break
+			}
+			fmt.Println("Error receiving message:", err)
+			continue
+		}
+
+		chatroom.Messages = append(chatroom.Messages, message)
+
+		html := bytes.Buffer{}
+		t := template.Must(template.New("chatroom").ParseFiles("templates/chatroom.html"))
+
+		if err := t.Execute(&html, chatroom); err != nil {
+			fmt.Println("Error executing template:", err)
+			return
+		}
+
+		for conn := range s.conns {
+			if err := websocket.Message.Send(conn, html.String()); err != nil {
+				fmt.Println("Error sending message:", err)
+				continue
+			}
+		}
+	}
+}
+
+type Page struct {
+	Chatroom *Chatroom
+}
+
+func NewPage(chatroom *Chatroom) *Page {
+	return &Page{
+		Chatroom: chatroom,
+	}
+}
+
+type Chatroom struct {
+	Messages []Message
+}
+
+func NewChatroom() *Chatroom {
+	return &Chatroom{
+		Messages: make([]Message, 0),
+	}
+}
+
+type Message struct {
+	Sender  string `json:"sender"`
+	Content string `json:"content"`
+}
+
+func NewMessage(sender, content string) *Message {
+	return &Message{
+		Sender:  sender,
+		Content: content,
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gergosabian/go-message
 
 go 1.22.0
+
+require golang.org/x/net v0.22.0
